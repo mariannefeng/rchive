ffbd1a782b4e9ec4d2fd2fe2784abea5231209fb
Marianne Feng <feng.marianne@gmail.com>
2026-05-27 22:13:35 -0400

    versions on versions on versions on versions

--- src/rchive/client/core.cljs
+++ src/rchive/client/core.cljs
@@ -48,19 +48,26 @@
 
 (defn app-view
   []
-  [:div {:tw "flex gap-2"}
+  [:div {:tw "flex gap-2 items-start"}
+   [:style
+    "@import url('https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100..900;1,100..900&display=swap');
+     #placard {
+       font-family: \"Libre Franklin\", sans-serif;
+     }"]
    [:style {:media "print"}
-    "print:hidden {display: none}"]
+    ".print\\:hidden {display: none}
+     .print\\:border-transparent {border-color: transparent}
+     .print\\:block {display: block !important}"]
 
    [:link {:href "/css/twstyles.css" :media "print" :rel "stylesheet"}]
 
-   [:div#form
-    [:form {:tw "print:hidden"}
+   [:div#form {:tw "print:hidden"}
+    [:form
 
      [text-input {:label "Title"
                   :key :placard/title}]
      [array-input {:label "Artists"
-                  :key :placard/artists}]
+                   :key :placard/artists}]
      [text-input {:label "Year"
                   :key :placard/year}]
      [text-input {:label "Materials"
@@ -72,15 +79,140 @@
                           (js/window.print))}
      "Print"]]
 
-   [:div {:tw "p-2 border text-red-500"
-          :style {:width "50mm"}}
-    [:div (:placard/title @placard)]
-    [:div (for [[i item] (map-indexed vector (:placard/artists @placard))]
-            ^{:key i}
-            [:div item])]
-    [:div (:placard/description @placard)]
-    [:div (:placard/materials @placard)]
-    [:div (:placard/year @placard)]]])
+   [:div {:tw "space-y-4 bg-gray-100"}
+    #_[:div#placard
+     {:tw "p-10 border relative m-4 print:border-transparent break-inside-avoid"
+      :style {:width "150mm"}}
+
+     [:div {:tw "italic font-light"} (interpose ", " (:placard/artists @placard))]
+
+     [:div {:tw "text-3xl pt-4 pb-1"}
+      [:span {:tw "font-bold"}
+       (:placard/title @placard) ", "]
+      [:span {:tw "font-thin"} (:placard/year @placard)]]
+
+     [:div {:tw "uppercase text-xs tracking-wider font-light"} (:placard/materials @placard)]
+
+     [:div {:tw "pt-4 font-light"} (:placard/description @placard)]
+
+     [:img {:tw "absolute top-10 -mt-2 -mr-2 right-10 w-22 h-22"
+            :src "/qr_sample.svg"}]
+
+
+     [:div {:tw "border-b border-r w-2 h-2 -top-2 -left-2 absolute hidden print:block"}]
+     [:div {:tw "border-t border-r w-2 h-2 -bottom-2 -left-2 absolute hidden print:block"}]
+     [:div {:tw "border-t border-l w-2 h-2 -bottom-2 -right-2 absolute hidden print:block"}]
+     [:div {:tw "border-b border-l w-2 h-2 -top-2 -right-2 absolute hidden print:block"}]]
+
+    
+    [:div#placard
+     {:tw "p-10 border relative m-4 pxrint:border-transparent break-inside-avoid bg-white  border-l-1em box-content"
+      :style {:width "150mm"}}
+
+     [:div {:tw "italic font-light"} (interpose ", " (:placard/artists @placard))]
+
+     [:div {:tw "text-3xl pt-4 pb-1"}
+      [:span {:tw "font-bold"}
+       (:placard/title @placard) ", "]
+      [:span {:tw "font-thin"} (:placard/year @placard)]]
+
+     [:div {:tw "uppercase text-xs tracking-wider font-light"} (:placard/materials @placard)]
+
+     [:div {:tw "flex gap-7 pt-4"}
+      [:div {:tw "font-light"} (:placard/description @placard)]
+      [:div {:tw "w-55"}
+       [:img {:tw "pt-2"
+              :src "/rc-qr.svg"}]]]
+
+     [:div {:tw "border-b border-r w-2 h-2 -top-2 -left-2 absolute hidden print:block"}]
+     [:div {:tw "border-t border-r w-2 h-2 -bottom-2 -left-2 absolute hidden print:block"}]
+     [:div {:tw "border-t border-l w-2 h-2 -bottom-2 -right-2 absolute hidden print:block"}]
+     [:div {:tw "border-b border-l w-2 h-2 -top-2 -right-2 absolute hidden print:block"}]]
+
+
+
+
+    [:div#placard
+     {:tw "p-10 border relative m-4 pxrint:border-transparent break-inside-avoid bg-white border-l-1em box-content"
+      :style {:width "120mm"}}
+
+     [:div {:tw "italic font-light"} (interpose ", " (:placard/artists @placard))]
+
+     [:div {:tw "text-3xl pt-4 pb-1"}
+      [:span {:tw "font-bold"}
+       (:placard/title @placard) ", "]
+      [:span {:tw "font-thin"} (:placard/year @placard)]]
+
+     [:div {:tw "uppercase text-xs tracking-wider font-light"} (:placard/materials @placard)]
+
+     [:div {:tw "pt-4 font-light"} (:placard/description @placard)]
+
+
+     [:div {:tw "flex justify-end"}
+      [:img {:tw "-mr-2 mt-20 w-22 h-22"
+             :src "/rc-qr.svg"}]]
+
+     [:div {:tw "border-b border-r w-2 h-2 -top-2 -left-2 absolute hidden print:block"}]
+     [:div {:tw "border-t border-r w-2 h-2 -bottom-2 -left-2 absolute hidden print:block"}]
+     [:div {:tw "border-t border-l w-2 h-2 -bottom-2 -right-2 absolute hidden print:block"}]
+     [:div {:tw "border-b border-l w-2 h-2 -top-2 -right-2 absolute hidden print:block"}]]
+
+
+    [:div {:tw "flex xborder m-4 relative bg-white"}
+     [:div {:tw "flex justify-end"}
+      [:img {:tw "w-71 h-71 p-10 -mr-12"
+             :src "/rc-qr.svg"}]]
+     [:div#placard
+      {:tw " p-10 relative print:border-transparent break-inside-avoid"
+       :style {:width "150mm"}}
+
+      [:div {:tw "italic font-light"} (interpose ", " (:placard/artists @placard))]
+
+      [:div {:tw "text-3xl pt-4 pb-1"}
+       [:span {:tw "font-bold"}
+        (:placard/title @placard) ", "]
+       [:span {:tw "font-thin"} (:placard/year @placard)]]
+
+      [:div {:tw "uppercase text-xs tracking-wider font-light"} (:placard/materials @placard)]
+
+      [:div {:tw "pt-4 font-light"} (:placard/description @placard)]
+
+
+      [:div {:tw "border-b border-r w-2 h-2 -top-2 -left-2 absolute hidden print:block"}]
+      [:div {:tw "border-t border-r w-2 h-2 -bottom-2 -left-2 absolute hidden print:block"}]
+      [:div {:tw "border-t border-l w-2 h-2 -bottom-2 -right-2 absolute hidden print:block"}]
+      [:div {:tw "border-b border-l w-2 h-2 -top-2 -right-2 absolute hidden print:block"}]]
+
+     ]
+    
+    #_[:div {:tw "flex xborder m-4 relative bg-white"}
+     [:div#placard
+      {:tw " p-10 relative print:border-transparent break-inside-avoid"
+       :style {:width "150mm"}}
+
+      [:div {:tw "italic font-light"} (interpose ", " (:placard/artists @placard))]
+
+      [:div {:tw "text-3xl pt-4 pb-1"}
+       [:span {:tw "font-bold"}
+        (:placard/title @placard) ", "]
+       [:span {:tw "font-thin"} (:placard/year @placard)]]
+
+      [:div {:tw "uppercase text-xs tracking-wider font-light"} (:placard/materials @placard)]
+
+      [:div {:tw "pt-4 font-light"} (:placard/description @placard)]
+
+
+      [:div {:tw "border-b border-r w-2 h-2 -top-2 -left-2 absolute hidden print:block"}]
+      [:div {:tw "border-t border-r w-2 h-2 -bottom-2 -left-2 absolute hidden print:block"}]
+      [:div {:tw "border-t border-l w-2 h-2 -bottom-2 -right-2 absolute hidden print:block"}]
+      [:div {:tw "border-b border-l w-2 h-2 -top-2 -right-2 absolute hidden print:block"}]]
+
+     [:div {:tw "flex justify-end"}
+      [:img {:tw "w-71 h-71 p-10"
+             :src "/rc-qr.svg"}]]]
+
+
+    ]])
 
 (defn render []
   (rdom/render [app-view]))
